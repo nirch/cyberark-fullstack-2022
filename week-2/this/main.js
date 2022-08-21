ef1cea3ca226bc5bfc023814e3e47683d0bf68dd
nirch <nirh2@yahoo.com>
2022-08-21 13:52:10 +0300

    +this getting lost

--- week-2/this/main.js
+++ week-2/this/main.js
@@ -12,20 +12,47 @@ function foo() {
 }
 foo();
 
+
+function sayHi3() {
+    console.log(`Hi ${this.name}`);
+}
+
 let person1 = {
     name: "John",
     sayHi: function() {
         console.log(`Hi ${this.name}`)
+        console.log(this);
     }
 }
-
-person1.sayHi();
-
-const y = person1.sayHi;
-y();
-
+// document.getElementById("my-btn").onclick = person1.sayHi;
+document.getElementById("my-btn").addEventListener("click", person1.sayHi)
 
 function elementClicked(element) {
     console.log(element);
     console.log(this);      // <-- note that here this is the global scope (window)
 }
+
+
+person1.sayHi();
+const sayHi2 = person1.sayHi;
+sayHi2();
+
+
+let person2 = {
+    name: "Sarah"
+}
+person2.sayHi = sayHi3;
+person2.sayHi();
+
+
+
+
+
+
+
+
+
+
+
+
+
