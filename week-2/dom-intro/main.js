f5ff3945d4d9d44b02b1b028425fa735efd17238
nirch <nirh2@yahoo.com>
2022-08-22 14:12:31 +0300

    dom intro - events + adding item to list

--- week-2/dom-intro/main.js
+++ week-2/dom-intro/main.js
@@ -5,6 +5,7 @@ function changeContent() {
 }
 
 function changeToWalla() {
+    console.log(arguments);
     // document.getElementById('my-a').href = "http://www.walla.co.il";
     document.getElementById('my-a').setAttribute("href", "http://www.walla.co.il")
     document.getElementById('my-a').innerHTML = "Go to Walla";
@@ -29,3 +30,63 @@ function changeColorInput() {
     const color = document.getElementById('my-color').value;
     document.getElementById('my-p').style.color = color;
 }
+
+
+
+// - Read Input             - V
+// - Click on a button      - V
+// - Add item to a list     - V
+// - Clean the input        - V
+
+document.getElementById('add-item-btn').addEventListener('click', function(event) {
+    // console.log(event.target.id);
+    console.log('clicked');
+
+    // Reading the input
+    const newItem = document.getElementById('shopping-input').value;
+
+    // Adding the new item to the list
+    const liElement = document.createElement('li');     // <li></li> -  not in the DOM yet
+    liElement.innerHTML = newItem;                    // <li>bla bla</li> - still not in the DOM
+    const ulElement = document.getElementById('shopping-list');
+    ulElement.appendChild(liElement);                   // adds the li to the DOM (under ul)
+
+    // ulElement.innerHTML += `<li>${newItem}</li>`;
+
+    // Cleaning the input
+    document.getElementById('shopping-input').value = "";
+
+});
+
+
+
+document.getElementById("grand-parent").addEventListener('click', function() {
+    console.log('grand-parent clicked');
+});
+
+document.getElementById("outer").addEventListener('click', function() {
+    console.log('outer clicked');
+})
+
+document.getElementById("inner").addEventListener('click', function(e) {
+    console.log('inner clicked');
+    // e.stopPropagation();
+})
+
+document.getElementById("outer-sibling").addEventListener('click', function() {
+    console.log('outer-sibling clicked');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
