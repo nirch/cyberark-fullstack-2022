9f835aceb9fd70c775cf93fc4e5b57435b5b2536
nirch <nirh2@yahoo.com>
2022-09-04 13:18:44 +0300

    promise vs callback

---
+++ week-4/promise-async-await/main.js
@@ -0,0 +1,46 @@
+function asyncPromise() {
+  return new Promise((resolve, reject) => {
+    if (new Date().getTime() % 2 === 0) {
+      resolve(10, 30);
+    } else {
+      reject('blablabla');
+    }
+  });
+}
+
+asyncPromise()
+  .then((data1, data2) => {
+    console.log(data1, data2);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+function getNumberOfBreeds(callback) {
+  const responsePromise = axios.get('https://dog.ceo/api/breeds/list/all');
+  responsePromise.then(response => {
+    callback(Object.keys(response.data.message).length);
+  });
+}
+
+getNumberOfBreeds(result => {
+  console.log(result);
+});
+
+function getNumberOfBreeds() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get('https://dog.ceo/api/breeds/list/all')
+      .then(response => {
+        resolve(Object.keys(response.data.message).length);
+      })
+      .catch(err => {
+        //
+        reject(err);
+      });
+  });
+}
+
+getNumberOfBreeds().then(numOfBreeds => {
+  console.log(numOfBreeds);
+});
